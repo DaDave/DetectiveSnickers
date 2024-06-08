f93142dfdcbdbd02d6921f44a549e3ac3323b5fc
DaDave <dave.schwadtke@mail.de>
2024-06-09 00:04:31 +0200

    Transition für TransitionLeftMission, TransitionRightMission, TransitionLeftPrivate erstellt

---
+++ Scenes/TransitionScenes/private_car.gd
@@ -0,0 +1,13 @@
+class_name PrivateCar extends CharacterBody2D
+
+@export_subgroup("Nodes")
+@export var movement_component: MovementComponent
+@export var animation_component: AnimationComponent
+
+@export_subgroup("Settings")
+@export var movement_speed: float
+
+func _physics_process(delta):
+	movement_component.handle_movement(self, movement_speed, 0.0)
+	animation_component.handle_move_animation(movement_speed, 0.0)
+	move_and_slide()
