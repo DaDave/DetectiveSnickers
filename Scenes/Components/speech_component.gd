4fcfe2caee2ac34e5a942073f51e3363be744185
Philipp Meyer <phmeyer555@gmail.com>
2024-06-08 13:09:24 +0200

    Überlappende Dialoge gefixt

--- Scenes/Components/speech_component.gd
+++ Scenes/Components/speech_component.gd
@@ -7,10 +7,15 @@ class_name SpeechComponent extends Node
 @export var text_speed: float = 0.05
 @export var text_timeout: float = 1.0
 
+var is_speaking = false
+
 func speak(text: String, initial_delay: float):
 	await get_tree().create_timer(initial_delay).timeout
-	for c in text:
-		speech_label.text = speech_label.text + c
-		await get_tree().create_timer(text_speed).timeout
-	await get_tree().create_timer(text_timeout).timeout
-	speech_label.text = ""
+	if !is_speaking:
+		is_speaking = true
+		for c in text:
+			speech_label.text = speech_label.text + c
+			await get_tree().create_timer(text_speed).timeout
+		await get_tree().create_timer(text_timeout).timeout
+		speech_label.text = ""
+		is_speaking = false
