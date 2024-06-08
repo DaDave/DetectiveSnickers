5294705def623776677380925d51c350b4100ba7
Philipp Meyer <phmeyer555@gmail.com>
2024-06-08 17:50:30 +0200

    Rotierendes CreepyPicture hinzugefügt

---
+++ Scenes/Interactables/creepy_picture.gd
@@ -0,0 +1,8 @@
+class_name CreepyPicture extends StaticBody2D
+
+@export_subgroup("Nodes")
+@export var sprite: Sprite2D
+
+func interact():
+	SpeechSignalBus.deputy_speak.emit("Spooky", 2.5, 1.0)
+	sprite.rotate(deg_to_rad(60))
