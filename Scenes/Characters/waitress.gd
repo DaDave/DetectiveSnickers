ed0c68ce0263daa2ad1fd1d93196c0801cf74fbd
Philipp Meyer <phmeyer555@gmail.com>
2024-06-09 01:53:08 +0200

    Level 2 kann jetzt abgeschlossen werden

--- Scenes/Characters/waitress.gd
+++ Scenes/Characters/waitress.gd
@@ -2,11 +2,24 @@ class_name Waitress extends CharacterBody2D
 
 @export_subgroup("Nodes")
 @export var speech_component: SpeechComponent
+@export var fridge: Fridge
 
 @export_subgroup("Settings")
-@export var speech_text: String = "I don't know what happened. I turned around and he was just sitting there."
-@export var deputy_text: String = "Interesting..."
+@export var speech_text = "I don't know what happened.\nI turned around and he was just sitting there."
+@export var deputy_text = "Interesting..."
+@export var speech_confession_text = "Ok, ok, I'll confess.\nYour impressive performance made it clear\nthat I won't be able to hide what I did."
+@export var speech_confession_text2 = "I loved him, but he didn't love me back.\nWhen I saw him smile while talking to that bitch\nI just snapped."
+@export var speech_confession_text3 = "If I can't have him, nobody will."
+@export var deputy_confession_text = "I really don't know how you do it, Detective Snickers.\nYou must be the most competent Detective\nin the history of solving murders."
 
 func interact():
-	SpeechSignalBus.queue_speech.emit(speech_component, speech_text)
-	SpeechSignalBus.deputy_speak.emit(deputy_text)
+	if !fridge.read:
+		SpeechSignalBus.queue_speech.emit(speech_component, speech_text)
+		SpeechSignalBus.deputy_speak.emit(deputy_text)
+	if fridge.read:
+		SpeechSignalBus.queue_speech.emit(speech_component, speech_confession_text)
+		SpeechSignalBus.queue_speech.emit(speech_component, speech_confession_text2)
+		SpeechSignalBus.queue_speech.emit(speech_component, speech_confession_text3)
+		SpeechSignalBus.deputy_speak.emit(deputy_confession_text)
+		await SpeechSignalBus.speech_queue_finished
+		SignalBus.case_closed.emit()
---
+++ Scenes/Interactables/fridge.gd
@@ -0,0 +1,26 @@
+class_name Fridge extends StaticBody2D
+
+@export_subgroup("Nodes")
+@export var sprite: AnimatedSprite2D
+
+@export_subgroup("Settings")
+@export var deputy_text = "Is that a bloody knife in the fridge?"
+@export var deputy_text2 = "That's really impressive, Detective Snickers!\nWho would've thought we'd find the murder weapon\nin there?"
+@export var deputy_text3 = "But who put it there?"
+
+var is_open = false
+var read = false
+
+func interact():
+	if is_open:
+		sprite.play("close")
+		is_open = false
+	else:
+		sprite.play("open")
+		is_open = true
+	await sprite.animation_finished
+	if is_open && !read:
+		SpeechSignalBus.deputy_speak.emit(deputy_text)
+		SpeechSignalBus.deputy_speak.emit(deputy_text2)
+		SpeechSignalBus.deputy_speak.emit(deputy_text3)
+		read = true
