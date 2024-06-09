e7e5b7a5e9929882d0843c4cbb0dce460a2f224c
Philipp Meyer <phmeyer555@gmail.com>
2024-06-09 21:00:50 +0200

    Animation für diagonales Laufen korrigiert

--- Scenes/Components/animation_component.gd
+++ Scenes/Components/animation_component.gd
@@ -12,18 +12,20 @@ func _ready():
 	sprite.frame_changed.connect(_on_frame_changed)
 
 func handle_move_animation(direction_horizontal: float, direction_vertical: float) -> void:
-	if up:
-		if direction_vertical != 0: sprite.play("run_up")
-		else: sprite.play("idle_up")
-	if down:
-		if direction_vertical != 0: sprite.play("run_down")
-		else: sprite.play("idle_down")
 	if left:
 		if direction_horizontal != 0: sprite.play("run_left")
 		else: sprite.play("idle_left")
-	if right:
+	elif right:
 		if direction_horizontal != 0: sprite.play("run_right")
 		else: sprite.play("idle_right")
+	elif up:
+		if direction_vertical != 0: sprite.play("run_up")
+		else: sprite.play("idle_up")
+	elif down:
+		if direction_vertical != 0: sprite.play("run_down")
+		else: sprite.play("idle_down")
+
+
 	
 	up = direction_vertical < 0
 	down = direction_vertical > 0
