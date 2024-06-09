514c3e3fcf2bc2c3b58f142081a05a5d32b87a6f
Philipp Meyer <phmeyer555@gmail.com>
2024-06-09 16:55:24 +0200

    Level 4 befüllt

---
+++ Scenes/Interactables/office_desk.gd
@@ -0,0 +1,12 @@
+class_name OfficeDesk extends StaticBody2D
+
+@export_subgroup("Settings")
+@export_multiline var deputy_text: String = "You're right, Detective Snickers.\nA detective of your skills shouldn't be\nbothered with lowly paperwork"
+@export_multiline var deputy_text2: String = "Go home,\nI'll take care of the reports"
+
+func interact():
+	rotate(deg_to_rad(90))
+	SpeechSignalBus.deputy_speak.emit(deputy_text)
+	SpeechSignalBus.deputy_speak.emit(deputy_text2)
+	await SpeechSignalBus.speech_queue_finished
+	SignalBus.case_closed.emit()
