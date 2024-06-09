53316c38cbfb2f6980632f041f6e0a5f2dfcdb40
DaDave <dave.schwadtke@mail.de>
2024-06-09 20:22:10 +0200

    Hupen eingefügt in Transition

--- Scenes/TransitionScenes/transition_right_mission.gd
+++ Scenes/TransitionScenes/transition_right_mission.gd
@@ -11,11 +11,11 @@ func first_transition():
 	await get_tree().create_timer(2.0).timeout
 	police_car.deputy_speak("Another day full of crime in this city...\nI'm really glad to have you with me, Detective Snickers.")
 	police_car.deputy_speak("Would you mind turning on the horn?\nI feel like this might be urgent.")
-	police_car.deputy_speak("Okay maybe I just like seeing you press\nSpacebar\nto interact with stuff")
+	police_car.deputy_speak("Okay maybe I just like seeing you press\nSPACEBAR\nto interact with stuff")
 	await SpeechSignalBus.speech_queue_finished
 	await InputSingleton.interact_pressed
 	police_car.snickers_speak("I AM DETECTIVE SNICKERS!")
-	#Hier Sirene einschalten
+	$HornAudioStreamPlayer2D.play()
 	await SpeechSignalBus.speech_queue_finished
 	await get_tree().create_timer(3.0).timeout
 	SceneSwitcherSingleton.switch_to_next_scene()
